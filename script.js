65db81485adfffa2eaabf30021267ffb0b2026fe
Joaov535 <joaovictorcruz535@gmail.com>
2023-05-16 13:12:09 -0300

    modularizando

--- script.js
+++ script.js
@@ -1,42 +1,39 @@
+// Praticando com prototypes
+
+
 const result = document.querySelector('.result'); // aparece o resultado no html
 const button = document.querySelector('button');
 
+
+function ValidaCPF(cpfEnviado) {
+    Object.defineProperty(this, 'cpfLimpo', {
+        enumerable: true,
+        get: function () {
+            return cpfEnviado.replace(/\D+/g, '')
+        }
+    });
+}
+
+
+
+ValidaCPF.prototype.valida = function () {
+    if (typeof this.cpfLimpo == 'undefined') return false;
+    if(this.cpfLimpo.length !== 11) return false;
+    if(this.isSequencia()) return false;
+    
+}
+
+ValidaCPF.prototype.isSequencia = function() {
+    
+}
+
 button.addEventListener('click', () => {
     const inputValue = document.querySelector('#input-display').value;
-    const cpfLimpo = inputValue.replace(/\D+/g, '');
-    const arrCpf = Array.from(cpfLimpo);
-    const cpfParcial = arrCpf.slice(0, 9);
+    const cpf = new ValidaCPF(inputValue);
 
-    
-    const contaRes = (cpf) => {
-        let inicial = cpf.length + 1;
-        let res = cpf.reduce((acc, item) => {
-            acc += (Number(item) * inicial);
-            inicial--;
-            return acc;
-        }, 0)
-        return res;
-    }
-
-    const geraDigito = (contaRes) => {
-        const restoDiv = contaRes % 11;
-        let digito = 11 - restoDiv;
-        if (digito > 9) {
-            digito = 0;
-        }
-
-        return String(digito);
-    }
-
-    const arrCpfDigUm = cpfParcial;
-    arrCpfDigUm.push(geraDigito(contaRes(cpfParcial)));
-
-    const arrCpfDigDois = arrCpfDigUm;
-    arrCpfDigDois.push(geraDigito(contaRes(arrCpfDigUm)));
-
-    if(arrCpfDigDois[9] === arrCpf[9] && arrCpfDigDois[10] === arrCpf[10]) {
-        result.innerHTML = 'CPF Válido!'
+    if (cpf.valida()) {
+        result.innerHTML = 'CPF Válido!';
     } else {
-        result.innerHTML = 'CPF Inválido!'   
+        result.innerHTML = 'CPF Inválido!';
     }
 });
