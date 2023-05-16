f0ba35b0957f3035b8b30d73e26793a4324b8276
Joaov535 <joaovictorcruz535@gmail.com>
2023-05-16 13:40:52 -0300

    modularizado

--- script.js
+++ script.js
@@ -1,9 +1,7 @@
-// Praticando com prototypes
+// Modularizando
 
-
-const result = document.querySelector('.result'); // aparece o resultado no html
-const button = document.querySelector('button');
-
+let result = document.querySelector('.result'); // aparece o resultado no html
+let button = document.querySelector('button');
 
 function ValidaCPF(cpfEnviado) {
     Object.defineProperty(this, 'cpfLimpo', {
@@ -14,22 +12,39 @@ function ValidaCPF(cpfEnviado) {
     });
 }
 
-
-
 ValidaCPF.prototype.valida = function () {
     if (typeof this.cpfLimpo == 'undefined') return false;
     if(this.cpfLimpo.length !== 11) return false;
-    if(this.isSequencia()) return false;
     
+    let cpfParcial = this.cpfLimpo.slice(0, -2);
+    let dig1 = this.criaDigito(cpfParcial);
+    let dig2 = this.criaDigito(cpfParcial + dig1);
+    let cpfGerado = cpfParcial + dig1 + dig2;
+
+    console.log(cpfGerado);
+
+    return cpfGerado === this.cpfLimpo;
 }
 
-ValidaCPF.prototype.isSequencia = function() {
-    
+ValidaCPF.prototype.criaDigito = function(cpfParcial) {
+    let cpfArr = Array.from(cpfParcial);
+    let inicial = cpfArr.length + 1;
+    let soma = cpfArr.reduce((acc, valor) => {
+        acc += (Number(valor) * inicial);
+        inicial--;
+        return acc;
+    },0);
+    let digito = 11 - (soma % 11);
+    if(digito > 9 ) {
+        digito = 0;
+    }
+    return String(digito);
 }
+
 
 button.addEventListener('click', () => {
-    const inputValue = document.querySelector('#input-display').value;
-    const cpf = new ValidaCPF(inputValue);
+    let inputValue = document.querySelector('#input-display').value;
+    let cpf = new ValidaCPF(inputValue);
 
     if (cpf.valida()) {
         result.innerHTML = 'CPF Válido!';
